6d122ce1550b98f6c9ab9dc8ffa4727485bb25b4
mathnerd3141 <doobahead@gmail.com>
2013-11-04 21:47:15 -0500

    Added local config
    [for my computer; should work for all XAMPP installations]

---
+++ lib/CONFIG.local.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * lib/CONFIG.local.php
+ * LHS Math Club Website
+ *
+ * This file is where information such as database connections, etc. are
+ * specified. YOU MAY EDIT THIS FILE.
+ */
+
+
+// DATABASE INFORMATION:
+$DB_SERVER =			'localhost';  // to speficy a port, use 'hostname:port'
+$DB_USERNAME =			'root';
+$DB_PASSWORD =			'';
+
+$PHP_MY_ADMIN_LINK =	'http://localhost/phpmyadmin/';
+
+
+// CUSTOM STYLING:
+$LOCAL_BORDER_COLOR = "#00FF00";
+
+?>
