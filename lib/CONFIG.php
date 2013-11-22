906f7b8e09aab2d73226a910e3a447522c233acb
mathnerd3141 <doobahead@gmail.com>
2013-11-22 00:04:23 -0500

    Changed mailbot gmail password
    Because that's a good idea.

--- lib/CONFIG.php
+++ lib/CONFIG.php
@@ -31,7 +31,7 @@ $PHP_MY_ADMIN_LINK =	'https://phpmyadmin.nearlyfreespeech.net/index.php';
 // SSL is used automatically.
 $EMAIL_ADDRESS =			'mailbot@lhsmath.org';
 $EMAIL_USERNAME =			'mailbot@lhsmath.org';
-$EMAIL_PASSWORD =			'x|c7Ev8x0(vygP><rXzu!#k8QJ@RhtUu9';
+$EMAIL_PASSWORD =			'JG~(Pew&Ft21!{UzE\'Hkjn@y:E=Ww^m`u:ch,&rDNUf(Gx%P!Q<}^3o]I{k}m*3';
 $SMTP_SERVER =				'smtp.gmail.com';
 $SMTP_SERVER_PORT =			'465';	//https://support.google.com/mail/answer/78775?hl=en
 									//The source of a month of failed email sendings when it changed 587 to 465 for whatever reason.
