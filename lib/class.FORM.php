bf18772cd7ef67cefb9e661e8d6b7efc346b7fed
Clive C <doobahead@gmail.com>
2014-10-13 21:36:09 -0400

    Trying to get FORM and Scorer to work

--- lib/class.FORM.php
+++ lib/class.FORM.php
@@ -1,21 +1,53 @@
 <?php //JQuery-style :)
-	class FORM{
-		protected $name, $method, $action, $xhr, $attribs;
-		protected $inputs;
-		public function __construct($name,$method=NULL,$action=NULL,$attribs=array()){
-			$this->name = strval($name);//$name is required.
-			$method = strtolower(strval($method));
-			switch($method){
-				//case 'xhrget': $this->method = 'get';$xhr = true; break; //XHR not yet supported!
-				//case 'xhrpost': $this->method = 'post';$xhr = true; break;
-				case 'get': $this->method = 'get';$xhr = false; break;
-				case 'post': default: $this->method = 'post';$xhr = false; break;
-			}
-			if($action===NULL || !is_string($action))$this->action = $_SERVER['REQUEST_URI'];
-			else $this->action = strval($action);
+	function hashEquals($a,$b){//Compares the *hashes* of two variables to mess with timing attacks. Because PHP hash_equals is inadequate.
+		if(!val('s',$a,$b))return false;
+		$m=microtime();
+		$str1 = sha1($a.$m.$b);
+		$str2 = sha1($b.$m.$a);
+		return hash_equals($str1, $str2);
+	}
+	function genRandStr($length=NULL){
+		if(!$length)$length=mt_rand(64,96);
+		$c = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';$cl = strlen($c);
+		$s = '';
+		for($i=0;$i<$length;$i++)$s.=$c[mt_rand(0,$cl-1)];
+		return $s;
+	}
+	function csrfVerify(){//Checks CSRF code validity, and returns whether to proceed. The return value is static. Erases 'ver'.
+		static $valid=NULL;
+		if(is_null($valid)){
+			if($_SESSION['ver'] && hashEquals($_POST['ver'],$_SESSION['ver']))$valid=true;
+			unset($_POST['ver'],$_SESSION['ver']);
 		}
-		public function createForm($name,$method=NULL,$action=NULL,$attribs=array()){
-			return new FORM($name,$method,$action,$attribs);
+		return $valid;
+	}
+	function csrfCode(/*$forceNew*/ /*$ver_name*/){//Returns randomly generated CSRF code. The return value is static.
+		static $code='';
+		if($_SESSION['ver']&&$code===$_SESSION['ver'])return $code;
+		
+		return ($code=$_SESSION['ver']=genRandStr());
+	}
+	
+	
+	class FormElement{
+		public $attribs;
+		public $prompt;
+		public $type;
+		public $value;
+		public $validator;
+		public function FormElement($attribs){
+			$this->attribs = $attribs;
+		}
+		public function HTML(){
+				if($attribs=='')return "&nbsp;"//blank spacer
+				
+				if($this->type == 'textarea')
+					return '<textarea '.attribsToString($this->attribs).'>'.$this->value.'</textarea>';
+				// elseif($this->type == 'select')
+					// $inputhtml = $attribs;//Not sure how to implement this
+				else{
+					return '<input '.attribsToString($this->attribs).'/>';
+				}
 		}
 		public function attribsToString($attribs){
 			$str = ' ';
@@ -28,71 +60,108 @@
 			}
 			return $str;
 		}
+	}
+	
+	class Form{
+		protected $name, $method, $action, $xhr;
+		protected $validators;
+		protected $inputs;
+		public function __construct($name,$method=NULL,$action=NULL,$attribs=array()){
+			$this->name = strval($name);//$name is required.
+			$method = strtolower(strval($method));
+			switch($method){
+				//case 'xhrget': $this->method = 'get';$xhr = true; break; //XHR not yet supported!
+				//case 'xhrpost': $this->method = 'post';$xhr = true; break;
+				case 'get': $this->method = 'get'; $xhr = false; 
+					if(isSet($_GET[$name]));
+					
+					break;
+				case 'post': default: $this->method = 'post'; $xhr = false;
+					
+					break;
+			}
+			if($action===NULL || !is_string($action))$this->action = $_SERVER['REQUEST_URI'];
+			else $this->action = strval($action);
+			
+			$this->validator(function($values){
+				if($_SESSION['ver'] && hashEquals($values['ver'],$_SESSION['ver'])){
+					unset($values['ver'],$_SESSION['ver']);
+					return true;
+				}
+				else{
+					return 'Error! Did you submit the same form twice?';
+				}
+			});
+		}
+		public function createForm($name,$method=NULL,$action=NULL,$attribs=array()){
+			return new FORM($name,$method,$action,$attribs);
+		}
 		public function HTML(){
 			$html="<form id='{$this->name}' name='{$this->name}' method='{$this->method}' action='{$this->action}' ".attribsToString($this->attribs)." >";
 			$html.='<table cellspacing="5px">'
-			foreach($inputs as $attribs){
-				if($attribs==''){$html.="<tr><td colspan='2'>&nbsp;</td></tr>";continue;}//blank spacer
-				
-				switch($attribs['type']){//HTML will ignore extraeneous attributes such as 'type' and 'value'
-					case 'textarea':
-						$inputhtml = '<textarea '.attribsToString($attribs).'>'.$attribs['value'].'</textarea>';
-						break;
-					//case 'select': $inputhtml = $attribs; break;//Not sure how to implement this.
-					default:
-						$inputhtml = '<input '.attribsToString($attribs).'/>';
-				}
-				
-				if($attribs['prompt'] && !empty($attribs['prompt']))//if no prompt, spans two columns of table, else has a prompt there
-					$html.="<tr><td>{$attribs['prompt']}</td><td>{$inputhtml}</td></tr>";
+			foreach($inputs as $input){
+				if(!empty($input->prompt))//if no prompt, spans two columns of table, else has a prompt there
+					$html.="<tr><td>{$input->prompt}</td><td>{$input->HTML()}</td></tr>";
 				else
-					$html.="<tr><td colspan='2'>{$inputhtml}</td></tr>";
+					$html.="<tr><td colspan='2'>{$input->HTML()}</td></tr>";
 			}
-			$html.='</table></form>';
+			$html.='</table>';
+			$html.='<input type="hidden" name="ver" value="'.csrfCode().'" />';
+			$html.='</form>';
 			return $html;
 		}
-		public function text($name,$prompt='',$hint='',$attribs=array()){
+		public function text($name,$attribs=array()){
 			$attribs['type']='text';
 			$attribs['name']=$name;
-			$attribs['prompt']=$prompt;
-			$attribs['hint']=$hint;
 			$this->inputs[]=$attribs;
 		}
-		public function textarea($name,$prompt='',$hint='',$attribs=array()){
+		public function textarea($name,$attribs=array()){
 			$attribs['type']='textarea';
 			$attribs['name']=$name;
-			$attribs['prompt']=$prompt;
-			$attribs['hint']=$hint;
-			$this->inputs[]=$attribs;
+			$this->inputs[]=new FormElement($attribs);
 		}
-		public function email($name,$prompt='Email Address:',$hint='',$attribs=array()){
+		public function email($name,$attribs=array()){
 			$attribs['type']='email';
 			$attribs['name']=$name;
-			$attribs['prompt']=$prompt;
-			$attribs['hint']=$hint;
-			$this->inputs[]=$attribs;
+			if(@!$attribs['prompt'])$attribs['prompt']='Email Address:';
+			$this->inputs[]=(new FormElement($attribs))
+				->validator(function($value){if(val('e',$value))return true;else return 'Invalid email.';});
 		}
-		public function pass($name,$prompt='Password:',$attribs=array()){
+		public function pass($name,$attribs=array()){
 			$attribs['type']='password';
 			$attribs['name']=$name;
-			$attribs['prompt']=$prompt;
-			$this->inputs[]=$attribs;
+			if(@!$attribs['prompt'])$attribs['prompt']='Password:';
+			$this->inputs[]=new FormElement($attribs);
 		}
 		public function blank(){//Adds a blank line
-			$this->inputs[]='';
+			$this->inputs[]=new FormElement('');
+		}
+		public function validator($func){
+			$validators[]=$func;
+		}
+		public function focus(){//Adds "focus" class to last input
+			@$inputs[count($inputs)-1]->attribs['class'].=' focus';
 		}
 	}
-	
-	/*
 	//----------PROPOSED USAGE----------//
-	$form = FORM::createForm('login','post')//can also xhrpost. default action is this page ($_SERVER['REQUEST_URI'])
-		->format('tabled')//other stuff?
+	$form = new Form('login','post')//can also xhrpost. default action is this page ($_SERVER['REQUEST_URI'])
+		//->format('tabled')//other stuff?
 		->email('email',{'prompt'=>'Email Address:','name'=>'email'})->focus()//by default focuses on last-added one, else focuses on supplied name
-		->pass('Password:','pass')//,$hint_value
+		->pass('pass',{})
 		->note('<a href="Password_Reset" class="small">Forgot your password?</a>')
 		->submit('Sign In')
-		->onsubmit(function($values){})//passes associative array of field name=>value to it, returns true or false.
-		->onsuccess(function(){alert('Successfully logged in.');})//If onsubmit returns false it will do something.
-		->onfailure(function(){echo $this->HTML();});//if onsubmit returns false it will do something else.
-	*/
+		//->validator()
+		->validator(function($values){
+			$id = DB::queryFirstField('SELECT id FROM users WHERE LOWER(email)=%s AND passhash=%s LIMIT 1',$email,hash_pass($values['pass']));
+			
+			if (is_null($id)) {
+				log_attempt($email, false);
+				show_login_form($email);
+				alert('Incorrect email address or password',-1);
+				return;
+			}
+			alert('Invalid username or password.',-1);
+			return false;
+		});//passes associative array of field name=>value to it
+	
 ?>
