61a6b253e0ffd970e9d8a0d4858fa16271898a5d
Clive Chan <cc@clive.io>
2015-11-23 17:16:42 -0500

    updated AMC for 2016

--- AMC.php
+++ AMC.php
@@ -6,18 +6,17 @@
 * For general AMC signup.
 */
 
-
-require_once 'lib/functions.php';
+$path_to_root = '';
+require_once $path_to_root.'lib/functions.php';
 
 page_title('AMC 10/12 Signup');
 ?>
 
 <h1>AMC 10/12 Signup</h1>
 
-<h3 style="text-align:center;color:red">Registration is now closed.</h3>
-<h5 style="text-align:center">We have a few extra tests, which will be handled on a case-by-case basis.
-Contact <a href="mailto:captains@lhsmath.org">cordero@lhsmath.org</a> if you haven't signed up yet but would like to participate.</h5>
-<br><br>
+<h4 style="text-align:center"><a href="http://goo.gl/forms/o80RSTkAQ1" rel="external">Registration Form</a></h4>
+<h4 style="text-align:center">Registration Deadline: Friday, Dec. 4.</h4>
+
 <div>
 	<p>The AMC (American Mathematics Competition) is an international examination in secondary school mathematics consisting of 
 		25 multiple choice problems to be solved in 75 minutes using concepts learned in school.
@@ -28,11 +27,12 @@ Contact <a href="mailto:captains@lhsmath.org">cordero@lhsmath.org</a> if you hav
 	<p>Two different versions of the contests are given on two different dates, the A and the B.
 	You can take it on both test dates, or just on one.</p>
 	<ul style="margin:0px;">
-		<li>The AMC 10A and 12A are on <b>Tuesday, February 3, 2015 at 3:00</b>.</li>
-		<li>The AMC 10B and 12B are on <b>Wednesday, February 25, 2015 at 3:00</b>.</li>
+		<li>The AMC 10A and 12A are on <b>Tuesday, February 2, 2015 at 1:30 PM</b>.</li>
+		<li>The AMC 10B and 12B are on <b>Wednesday, February 17, 2015</b>. <b style="color:red">LHS will not be offering the AMC B (RSM will be).</b></li>
 	</ul>
 </div>
 <div>
 	<p>See <a href="http://www.maa.org/math-competitions/amc-contests">the AMC website</a> for more information on this series of contests.
 	If you have any questions, you can contact us at <a href="mailto:amc@lhsmath.org">amc@lhsmath.org</a>.</p>
 </div>
+
