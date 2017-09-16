c5191a248f4ce0700be0d85ad95782173f73c2df
Louie Golowich <louie_g@verizon.net>
2017-09-16 09:42:14 -0400

    Took down AMC and put up MAML on website.

--- .lib/functions.template.php
+++ .lib/functions.template.php
@@ -235,7 +235,7 @@ $main_navbar = array( //Name => Page path, or if it's the same you can omit the 
 	'X'=>[
 		'LMT',
 		'',
-		'AMC',
+		'MAML',
 		'',
 		'Calendar',
 		'Contests',
@@ -248,7 +248,7 @@ $main_navbar = array( //Name => Page path, or if it's the same you can omit the 
 	'ARL'=>[
 		'LMT',
 		'',
-		'AMC',
+		'MAML',
 		'',
 		'Contact',
 		'About',
