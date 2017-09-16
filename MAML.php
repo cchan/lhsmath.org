a2292faf72aff808240b98491e53e30f82ca466b
Louie Golowich <louie_g@verizon.net>
2017-09-16 09:36:26 -0400

    Updated MAML signup.

--- MAML.php
+++ MAML.php
@@ -9,12 +9,12 @@
 
 require_once '.lib/functions.php';
 
-page_title('MAML Signup 2016-2017');
+page_title('MAML Signup 2017-2018');
 ?>
 
-<h1>MAML Signup 2016-2017</h1>
+<h1>MAML Signup 2017-2018</h1>
 
-<h3 style="text-align:center;"><a href="https://goo.gl/forms/DQYc9U5nSfugKXAn2">https://goo.gl/forms/DQYc9U5nSfugKXAn2</a></h3>
+<h3 style="text-align:center;"><a href="https://goo.gl/forms/m2PLo2WFS1j09w7z1">https://goo.gl/forms/m2PLo2WFS1j09w7z1</a></h3>
 <div>
 	<p>
 	The MAML (Massachusetts Association of Mathematics Leagues) is an MA-wide high school math contest, with 
